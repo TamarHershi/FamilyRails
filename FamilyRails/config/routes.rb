f1d9b5a1df232f897e133521dd920c0ad3b95b76
Tammy <misstamar@gmail.com>
2015-11-24 14:38:58 -0800

    decided to create user controller

--- FamilyRails/config/routes.rb
+++ FamilyRails/config/routes.rb
@@ -6,7 +6,6 @@ Rails.application.routes.draw do
 
   resources :children do
     resources :toys do
-
     end
   end
   # The priority is based upon order of creation: first created -> highest priority.
