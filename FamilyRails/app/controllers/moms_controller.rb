17719be1fb7973bd4166a1c747c54ab96c1a8d66
Tammy <misstamar@gmail.com>
2015-11-24 20:18:11 -0800

    added edit / create/ new options for moms

--- FamilyRails/app/controllers/moms_controller.rb
+++ FamilyRails/app/controllers/moms_controller.rb
@@ -1,15 +1,46 @@
 class MomsController < ApplicationController
 
+  def find_mom
+    @mom = Mom.find(params[:id])
+  end
+
   def index
     @moms = Mom.all
   end
 
   def show
-    @mom = Mom.find(params[:id])
+    find_mom
   end
 
   def show_children
     mom = Mom.find(params[:mom_id])
     @kids = mom.children
   end
+
+  def new
+    @mom = Mom.new
+  end
+
+  def edit
+    find_mom
+  end
+
+  def create
+    Mom.create(mom_params[:mom])
+    redirect_to moms_path
+  end
+
+  def update
+    find_mom
+    @mom.update(mom_params[:mom])
+    redirect_to show
+  end
+
+
+  private
+
+  def mom_params
+    params.permit(mom: [:name, :job, :age])
+  end
+
 end
